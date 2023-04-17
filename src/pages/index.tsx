b2933f6bcf1fb59b6bb8e313c4a225ab3fd3fc7f
Sergio Francisco <serjo@SerjoNitro>
2023-04-17 17:05:18 -0300

    Coloca a imagem do bing como background

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,26 @@
+import getURl from "@/services/backgroundImage"
+import Image from "next/image"
+import { useState, useEffect } from "react"
 export default function Home() {
+  const [backgroundImage, setBackgroundImage] = useState("")
+  const getBackgroundImage = async () => {
+    const url = await getURl()
+    setBackgroundImage(url)
+    console.log(url)
+  }
+   useEffect(() => {
+    getBackgroundImage()
+   }, [])
+   const styling = {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: "cover",
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+   } as const
   return (
-    <main>
-     
-    </main>
+    <main style={styling}>
+      <h1>Background Image</h1>
+    </main> 
   )
 }
--- src/services/backgroundImage.ts
+++ src/services/backgroundImage.ts
@@ -1,16 +1,11 @@
-async function getURl() {
-    const URL = `https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=pt-US`;
+import axios from 'axios';
+export default async function getURl() {
+    const URL = `https://bing.biturl.top/`;
     try {
-      const response = await fetch(URL);
-      const data = await response.json();
-      return data.images[0].url;
+      const response = await axios.get(URL);
+      const data = response.data;
+      return data.url;
     } catch (err) {
       console.log(err);
     }
-  }
-
-async function getBackgroundImage() {
-    const url = await getURl();
-    const imageURL = `https://www.bing.com${url}`;
-    return imageURL;
   }
