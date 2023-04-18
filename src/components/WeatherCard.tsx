b055cfdcad1fa39f97d09abefb3b66a72d70d41a
Sergio Francisco <serjo@SerjoNitro>
2023-04-18 00:01:02 -0300

    Cria interface do retorno da API de tempo e comenta as variaveis

--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -22,3 +22,48 @@ export default function WeatherCard() {
     <div>nada</div>
   )
 }
+
+// interface Weather 
+//   {
+//     "coord": {
+//         "lon": number,
+//         "lat": number
+//     },
+//     "weather": [
+//         {
+//             "id": number,
+//             "main": string, // Grupo do clima
+//             "description": string, // descrição do clima
+//             "icon": string // icon id (Lembrar o que fazer com isso)
+//         }
+//     ],
+//     "base": string,
+//     "main": {
+//         "temp": number,  // temperatura
+//         "feels_like": number, // sensação térmica
+//         "temp_min": number,  // temperatura minima
+//         "temp_max": number,  // temperatura maxima
+//         "pressure": number,  // pressão atmosferica
+//         "humidity": number   // umidade do ar
+//     },
+//     "visibility": number, // visibilidade
+//     "wind": {
+//         "speed": number, // velocidade do vento
+//         "deg": number    // direção do vento
+//     },
+//     "clouds": {
+//         "all": number   // porcentagem de nuvens
+//     },
+//     "dt": number,
+//     "sys": {
+//         "type": number,
+//         "id": number,
+//         "country": string, // codigo do pais
+//         "sunrise": TimeRanges, // horario do nascer do sol
+//         "sunset": TimeRanges  // horario do por do sol
+//     },
+//     "timezone": number, // diferença de horario em segundos com o UTC
+//     "id": number,
+//     "name": string,
+//     "cod": number // codigo de resposta, 200 = ok
+// }
--- src/services/weatherService.ts
+++ src/services/weatherService.ts
@@ -2,7 +2,7 @@ const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
 
 async function getWeatherFromAPI(lat: number, lon: number) {
     //lat = latitude, lon = longitude, cnt = number of days, appid = API key 
-    const URL = `${BASE_URL}?lat=${lat}&lon=${lon}&cnt=3&units=metric&appid=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}`;
+    const URL = `${BASE_URL}?lat=${lat}&lon=${lon}&lang=pt_br&units=metric&appid=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}`;
     try{
         const response = await fetch(URL);
         const data = await response.json();
