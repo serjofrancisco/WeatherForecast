12477ce3e80e5c80e2d161b26197bb40c31af4a6
Sergio Francisco <spinheiro.lf@gmail.com>
2023-04-27 16:03:09 -0300

    aplica estilo ao card de tempo atual

--- src/components/CurrentWeatherCard.tsx
+++ src/components/CurrentWeatherCard.tsx
@@ -29,27 +29,54 @@ export default function CurrentWeatherCard() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [location]);
 
+  const stylingOutside = {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: '1.5rem',
+    color: 'white',
+    textShadow: '2px 2px 4px #000000',
+  } as const;
+
+  const titleStyle = {
+    fontSize: '2rem',
+    color: 'white',
+    textShadow: '2px 2px 4px #000000',
+    textAlign: 'center',
+  } as const;
   return (
     <div>
       { currentWeather?.main ? (
-        <div>
-          <h1>Clima Atual</h1>
-          <p>
-            {`${
-              firstLetterToUpperCase(currentWeather.weather[0].description)
-            }`}
+        <>
+          <h1 style={titleStyle}>Clima Atual</h1>
+          <div style={stylingOutside}>
+            <div>
+              <p>
+                {`${firstLetterToUpperCase(currentWeather.weather[0].description)}`}
 
-          </p>
-          { iconURL && (
-          <Image src={iconURL} alt={currentWeather.weather[0].description} width={30} height={30} />
-          )}
-          <p>{`Temperatura Atual: ${currentWeather.main.temp}`}</p>
-          <p>{`Sensação Térmica: ${currentWeather.main.feels_like}`}</p>
-          <p>{`Temperatura Máxima: ${currentWeather.main.temp_max}`}</p>
-          <p>{`Temperatura Mínima: ${currentWeather.main.temp_min}`}</p>
-          <p>{`Umidade: ${currentWeather.main.humidity}`}</p>
-          <p>{`Velocidade do Vento: ${currentWeather.wind.speed}Km/h`}</p>
-        </div>
+              </p>
+              {iconURL && (
+              <Image
+                src={iconURL}
+                alt={currentWeather.weather[0].description}
+                width={30}
+                height={30}
+              />
+              )}
+              <p>{`Temperatura Atual: ${currentWeather.main.temp}`}</p>
+              <p>{`Sensação Térmica: ${currentWeather.main.feels_like}`}</p>
+            </div>
+            <div>
+
+              <p>{`Temperatura Máxima: ${currentWeather.main.temp_max}`}</p>
+              <p>{`Temperatura Mínima: ${currentWeather.main.temp_min}`}</p>
+              <p>{`Umidade: ${currentWeather.main.humidity}`}</p>
+              <p>{`Velocidade do Vento: ${currentWeather.wind.speed}Km/h`}</p>
+            </div>
+          </div>
+
+        </>
       ) : <h1>Carregando...</h1>}
     </div>
   );
